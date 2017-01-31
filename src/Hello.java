9e407715245af57ef51703d86b9c6903801bf1dd
w3iiipu <w3iiipu@gmail.com>
2017-01-31 13:16:24 -0500

    practice

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * The HelloWorld class implemens an application that simply prints "hello, Wei!" to standard output.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Wei!");
+    }
+}
